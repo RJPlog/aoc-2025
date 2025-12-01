13242a9152623deb4ab785bfafa8a47fcfa93909
RJPlog <tfrenz67@gmail.com>
2025-12-01 23:07:58 +0100

    both parts of the puzzle solved
    Refactor password calculation logic and update output.

--- day2501_1_2.kt
+++ day2501_1_2.kt
@@ -14,17 +14,37 @@ fun passWord(in1: Int): Int {
         }
         if (position == 0) count +=1
     }
-
-
     return count
+}
 
+fun passWord2(in1: Int): Int {
+    var position = 50
+    var count = 0
+
+    File("day2501_puzzle_input.txt").forEachLine {
+        var steps = it.drop(1).toInt()
+        if (it.first() == 'R') {
+			repeat(steps) {
+				position += 1
+				if (position == 100) position = 0
+				if (position == 0) count += 1
+			}
+        } else {
+			repeat(steps) {
+				position -= 1
+				if (position == -1) position = 99
+				if (position == 0) count += 1					
+			}
+        }
+    }
+    return count
 }
 
 fun main() {
     var t1 = System.currentTimeMillis()
 
     var solution1 = passWord(1)
-    var solution2 = passWord(2)
+    var solution2 = passWord2(2)
 
 // print solution for part 1
     println("*******************************")
@@ -35,10 +55,10 @@ fun main() {
     println()
 
 // print solution for part 2
-//  println("*******************************")
-//  println("Solution for part2")
-//  println("   $solution2 is their similarity score")
-//  println()
+    println("*******************************")
+    println("Solution for part2")
+    println("   Using password method 0x434C49434B $solution2 is the password to open the door")
+    println()
 
     t1 = System.currentTimeMillis() - t1
     println("puzzle solved in ${t1} ms")
