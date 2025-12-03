f8cf650dc084775694e6a9913a3ac90a447c8d27
RJPlog <tfrenz67@gmail.com>
2025-12-03 15:26:00 +0100

    Refactor solution for part 2

--- day2503_1_2.kt
+++ day2503_1_2.kt
@@ -3,19 +3,15 @@ import kotlin.math.*
 
 fun lobby(in1: Int): Long{
     var joltageTotal = 0L
-    var test = listOf("987654321111111", "811111111111119", "234234234234278", "818181911112111")
-    
+    val test = listOf("987654321111111", "811111111111119", "234234234234278", "818181911112111")
+      
     test.forEach {
  		var joltage = ""
         var substring = it
 		for (i in in1-1 downTo 0) {
-        	substring = substring.dropLast(i)
-            joltage += substring.toList().sortedDescending()[0] 
-            val cutIndex = substring.indexOf(substring.toList().sortedDescending()[0])
-            substring =  it.drop(cutIndex+1)  // das wird immer wieder neuüberschrieben? 
-            // -> first correction needed: substring instead of it
-            // -> second correction: wenn nicht mehr genug da sind, müssen die in der vorhandenen 
-            // reihenfolge genommen werden! -> new concept? Worked for part one only accidentially            
+            joltage += substring.dropLast(i).toList().sortedDescending()[0] 
+			val cutIndex = substring.dropLast(i).indexOf(joltage.takeLast(1))
+            substring =  substring.drop(cutIndex+1) 
     	}
         joltageTotal += joltage.toLong()
     }
@@ -39,7 +35,7 @@ fun main() {
 // print solution for part 2
     println("*******************************")
     println("Solution for part2")
-    println("   $solution2 is the total output joltage")  //173084479160332 is to high
+    println("   $solution2 is the total output joltage")  //173084479160332 is to high   
     println()
 
     t1 = System.currentTimeMillis() - t1
