2e4413a93c14e23ddff93814cf6c100c1e49ed80
RJPlog <tfrenz67@gmail.com>
2025-12-05 12:35:46 +0100

    solution for part 2

--- day2505_1_2.kt
+++ day2505_1_2.kt
@@ -1,7 +1,7 @@
 import java.io.File
 import kotlin.math.*
 
-fun cafeteria(in1: Int): Int{
+fun cafeteria(in1: Int): Long{
     var puzzleInput = mutableListOf<String>()
 	
 	File("day2505_puzzle_input.txt").forEachLine {
@@ -10,48 +10,70 @@ fun cafeteria(in1: Int): Int{
 	puzzleInput.remove("")
     
     val (IDRangesRaw, ingredients) = puzzleInput.partition {it.contains("-")}    
-    var IDRanges = IDRangesRaw.map {it -> Pair(it.split("-")[0].toLong(),it.split("-")[1].toLong())}.sortedBy {it.first}
+    val IDRanges : MutableList<Pair<Long,Long>> = IDRangesRaw.map {it -> Pair(it.split("-")[0].toLong(),it.split("-")[1].toLong())}.sortedBy {it.first}.toMutableList()
     
-    // here may come the need to consolidate ID ranges
-    
-    var countFresh = 0
+    // consolidate overlapping ID ranges
+	var IDRangesCon = mutableListOf<Pair<Long,Long>>()
+	
+	var newRangeFirst = IDRanges[0].first
+	var newRangeSecond = IDRanges[0].second
 
-    ingredients.forEach {
+	for (i in 1..IDRanges.size-1) {
+		var RangeFirst = IDRanges[i].first
+		var RangeSecond = IDRanges[i].second
+ 		
+		if (RangeFirst <= newRangeSecond) {
+			newRangeSecond = maxOf(newRangeSecond,RangeSecond)
+		}
+		if (newRangeSecond < RangeFirst) {
+			IDRangesCon.add(Pair(newRangeFirst, newRangeSecond))
+			newRangeFirst = RangeFirst
+			newRangeSecond = RangeSecond
+		}
+	}
+	if(!IDRangesCon.contains(Pair(newRangeFirst, newRangeSecond))) IDRangesCon.add(Pair(newRangeFirst, newRangeSecond))
+    
+	// calculate results
+    var countFresh = 0L
+	
+	if (in1 == 1) {
+		ingredients.forEach {
         val ingredient = it.toLong()
-		var countDouble = false
-        IDRanges.forEach {
-            if (ingredient <= it.second && ingredient >= it.first && !countDouble) {
-                countFresh += 1
-				countDouble = true
+        IDRangesCon.forEach {
+            if (ingredient <= it.second && ingredient >= it.first) {
+                countFresh += 1L
             }
         }
-    }
-      
+    } 
     return countFresh
+	} else if (in1 == 2) {
+        IDRangesCon.forEach {
+            countFresh += it.second-it.first+1L
+        }
+        return countFresh
+    }
+	return -1L
 }
+
 
 fun main() {
     var t1 = System.currentTimeMillis()
-
-	val solution1 = cafeteria(1)
-    
-
+ 
 //  solution for part 1
+	val solution1 = cafeteria(1)
     println("*******************************")
     println("--- Day 5: Cafeteria ---")
     println("*******************************")
     println("Solution for part1")
-    println("   $solution1 ingredient IDs are fresh")   //824 is to high -> guess some of the ingrdients are counted double
-    println()
+    println("   $solution1 ingredient IDs are fresh")
+    println() 
 
-    /*
 //  solution for part 2
-	val solution2 = cafeteria(1)
+	val solution2 = cafeteria(2)
     println("*******************************")
-    println("Solution for part2")
-    println("   $solution2 ")  
+    println("Solution for part2")        
+    println("   $solution2 IDs are considered as fresh ")
     println()
-    */
 
     t1 = System.currentTimeMillis() - t1
     println("puzzle solved in ${t1} ms")
