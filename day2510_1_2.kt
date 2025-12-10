c59ebc6515b6f1be9042f2b2bb9d695538622ab6
RJPlog <tfrenz67@gmail.com>
2025-12-10 20:42:17 +0100

    develop idea for part1

---
+++ day2510_1_2.kt
@@ -0,0 +1,102 @@
+import java.io.File
+import kotlin.math.*
+
+var permutationList = mutableListOf<String>()
+
+fun permutations(in1: Int, in2: String) {
+    val range = 2
+    if (in1 == 0) {
+        for (i in 0..range) {
+            permutationList.add(in2 + i.toString())
+        }
+	} else {
+        for (i in 0..range) {
+            permutations(in1-1, in2 +i.toString() )
+        }
+    }
+}
+
+fun factory(in1: Int): Int {
+    val puzzleInput = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"""
+    
+    
+    val indicatorLights = mutableListOf<String>()
+    val buttonWiring = mutableListOf<String>()
+    puzzleInput.split("\n").forEach{
+        indicatorLights.add(it.substringBefore("]").drop(1))
+        buttonWiring.add(it.substringAfter("] ").substringBefore(" {"))
+    }
+    println(indicatorLights)
+    println(buttonWiring)
+    
+    var totalButtonsPressed = 0
+    for (i in 0..indicatorLights.size-1) {
+        
+        var currentButtonList = buttonWiring[i].split(" ").map {it.drop(1).dropLast(1)}
+        println(currentButtonList)
+        
+        // create all permutations
+        permutationList.clear()
+        permutations(buttonWiring[i].split(",").size,"")
+        //println(permutationList)     
+             
+        // calculate lightbar result for each permutation
+        
+        var minResult = 0
+        permutationList.forEach {
+        //listOf("111000","010102", "000011").forEach{
+           var switchResult = MutableList(indicatorLights[i].length) {0}
+           for(j in 0..it.length -1) {
+               val nPressed = it[j].toString().toInt()
+               currentButtonList[j].split(",").forEach {
+                   switchResult[it.toString().toInt()] +=nPressed
+                   //println("   j: $j it: $it nPressed: $nPressed result $switchResult")
+               }
+               
+           }
+           var  result = switchResult.map {it -> if (it % 2 == 0) '.' else '#'}.joinToString("")
+            //println("$it    $switchResult $result")
+           if (result == indicatorLights[i]) {
+               		if (minResult == 0) {
+                        minResult = it.fold(0) {a, n -> a + n.toString().toInt()}
+                    } else {
+                        minResult = minOf(minResult, it.fold(0) {a, n -> a + n.toString().toInt()})
+                    }
+                    // calc min number of pressed buttons
+                } 
+           
+           //println("minResult $minResult")
+           
+           
+        }
+        totalButtonsPressed += minResult
+        println("totalButtonsPressed $totalButtonsPressed")
+
+    }
+    
+	return totalButtonsPressed
+}
+
+fun main() {
+    
+    var t1 = System.currentTimeMillis()
+ 
+//  solution for part 1
+	val solution1 = factory(1)
+    println("*******************************")
+    println("--- Day 10: Factory ---")
+    println("*******************************")
+    println("Solution for part1")
+    println("   $solution1")
+    println() 
+
+/*/  solution for part 2
+	val solution2 = factory(2)
+    println("*******************************")
+    println("Solution for part2")        
+    println("   $solution2")
+    println() */
+
+    t1 = System.currentTimeMillis() - t1
+    println("puzzle solved in ${t1} ms")
+}
