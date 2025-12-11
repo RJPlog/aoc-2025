82928d95ae1dfe74978541e2faba76a5c79ae765
RJPlog <tfrenz67@gmail.com>
2025-12-11 14:19:41 +0100

    recursiv approach for part 2
    calculation takes much to long

--- day2507_1_2.kt
+++ day2507_1_2.kt
@@ -1,84 +1,76 @@
 import java.io.File
 import kotlin.math.*
 
-fun laboratory(puzzleInput: MutableList<MutableList<Char>>, in2: Int): Long{
-    //var puzzleInput = mutableListOf<MutableList<Char>>()
-	
-	var width = puzzleInput[0].size
-	var height = puzzleInput.size
-	
-	// addon for part2
-	var pathCount = MutableList(height) {MutableList<Long>(width) {0L}}
-	pathCount[0][puzzleInput[0].indexOf('S')] = 1L
-	
-	var splitCount = 0L
-	
-	for (y in 1..height-1) {
-		for (x in 0..width-1) {
-			if (puzzleInput[y-1][x] == '|' || puzzleInput[y-1][x] == 'S') {
-				if (puzzleInput[y][x] == '.') {
-					puzzleInput[y][x] = '|'
-					// add on for part2
-					pathCount[y][x] += pathCount[y-1][x]
-					
-				} else if (puzzleInput[y][x] == '^') {
-					puzzleInput[y][x-1] = '|'
-					puzzleInput[y][x+1] = '|'
-					splitCount += 1L
-					
-					// addon for part2					
-					pathCount[y][x-1] += pathCount[y-1][x] //that was a tricky one, if you are coming from left then already in previous round the vertical number was added - don't do it twice
-					pathCount[y][x+1] += pathCount[y-1][x] + pathCount[y-1][x+1]
-				}
-			}
-		}
-	}
-	
-	// addon for part2
-	if (in2 == 2) return pathCount[height - 1].sum()
+var puzzleInputMap = mutableMapOf<String,String>()
 
-	return splitCount
+fun reactor (in1: Int): Int {
+    var pathList = mutableListOf<String>()
+	pathList.add("you")
+
+    
+    var searchPath = true
+    while (searchPath) {
+        for (i in 0..pathList.size-1) {
+            if (pathList[i] != "out") {
+                puzzleInputMap.getValue(pathList[i]).split(" ").forEach {
+                    pathList.add(it)
+                }
+                pathList[i] = ""
+            } 
+        }
+        while (pathList.contains("")) {
+            pathList.removeAt(pathList.indexOf(""))
+        }
+ 
+        if (pathList.size == pathList.count {it == "out"}) searchPath = false
+    }
+	return pathList.size
 }
 
-// first idea for part2, works for example but for real data takes way to long (if really working :-))
-fun quantumTachyonManifold(puzzleInput: MutableList<MutableList<Char>>, xPos: Int, yPos: Int): Int{
-	if (yPos == puzzleInput.size-1) return 1
-	if(puzzleInput[yPos][xPos] == '.'){
-		return quantumTachyonManifold(puzzleInput, xPos, yPos + 1)
-	} else if (puzzleInput[yPos][xPos] == '^') {
-	    return quantumTachyonManifold(puzzleInput, xPos -1 , yPos + 1) + quantumTachyonManifold(puzzleInput, xPos +1, yPos + 1)
+fun reactor2(in1: String, in2: Int): Int {
+	//println("Aufruf mit $in1 und $in2")
+	var result = 0
+	if (in1 != "out") {
+		//println("   ${puzzleInputMap.getValue(in1)} ${puzzleInputMap.getValue(in1).split(" ").size}")
+		puzzleInputMap.getValue(in1).split(" ").forEach {
+			//println("      ${it}")
+			var patternDetected = 0
+			if (it == "dac" || it == "fft") patternDetected = 1
+			result += reactor2(it, in2 + patternDetected)
+		}
+		return result
 	}
-	return 0
+
+	if (in2 == 2) {
+		println()
+		return 1
+	} else {
+		return 0
+	}
 }
 
 fun main() {
+    
     var t1 = System.currentTimeMillis()
-	
-	var puzzleInput = mutableListOf<MutableList<Char>>()
-	File("day2507_puzzle_input.txt").forEachLine {
-		puzzleInput.add(it.toMutableList())	
-	}
  
-//  solution for part 1
-	val solution1 = laboratory(puzzleInput, 1)
+	File("day2511_puzzle_input.txt").forEachLine {
+		puzzleInputMap.put(it.split(": ")[0], it.split(": ")[1])
+    }
+	
+/*/  solution for part 1
+	val solution1 = reactor(1)
     println("*******************************")
-    println("--- Day 7: Laboratories ---")
+    println("--- Day 11: Reactor ---")
     println("*******************************")
     println("Solution for part1")
-    println("   The beam will be split $solution1 times")
-    println()
-	
-	puzzleInput.clear()
-	File("day2507_puzzle_input.txt").forEachLine {
-		puzzleInput.add(it.toMutableList())	
-	}
+    println("   $solution1")
+    println() */
 
 //  solution for part 2
-	//val solution2 = quantumTachyonManifold(puzzleInput, puzzleInput[0].indexOf('S'), 1)
-	val solution2 = laboratory(puzzleInput, 2)
+	val solution2 = reactor2("svr",0)
     println("*******************************")
     println("Solution for part2")        
-    println("   $solution2 different timelines would a single tachyon particle end up on")
+    println("   $solution2")
     println() 
 
     t1 = System.currentTimeMillis() - t1
