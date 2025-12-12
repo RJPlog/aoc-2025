ae4b1c96f1e3ad99d563eeaf424ba626bb06ce88
RJPlog <tfrenz67@gmail.com>
2025-12-12 08:13:23 +0100

    try another idea for part2

--- day2511_1_2.kt
+++ day2511_1_2.kt
@@ -1,7 +1,16 @@
 import java.io.File
 import kotlin.math.*
 
+data class Node (
+	var id: String,
+    var count: Long,
+    var reached: Int
+)
 
+data class NodeRed (
+	var id: String,
+    var reached: Int
+)
 
 fun reactor (in1: Int): Int {
     val puzzleInput = """aaa: you hhh
@@ -50,6 +59,91 @@ iii: out"""
 	return pathList.size
 }
 
+fun reactor2 (in1: Int): Long {
+    val puzzleInput = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"""
+    
+    var puzzleInputMap = mutableMapOf<String,String>()
+    puzzleInput.split("\n").forEach {
+            puzzleInputMap.put(it.split(": ")[0], it.split(": ")[1])
+    }
+
+	println("puzzleInputMap $puzzleInputMap")
+    
+	var pathList = mutableListOf<Node>()
+	pathList.add(Node("svr",1,0))
+
+	var pathSearch = true
+    var n = 0
+    while (pathSearch) {
+        pathSearch = false
+        n += 1
+        println(n)
+        for (i in 0..pathList.size-1) {
+            val id = pathList[i].id
+            if (id != "out") {
+                val count = pathList[i].count
+                var reached = pathList[i].reached
+                pathList[i] = Node("",0,0)
+                puzzleInputMap.getValue(id).split(" ").forEach {
+                    if (it =="fft") reached += 1 
+                    if (it == "dac") reached += 10
+                    pathList.add(Node(it, count, reached))
+                }
+            }
+        }
+        while (pathList.contains(Node("",0,0))) {
+            pathList.removeAt(pathList.indexOf(Node("",0,0)))
+        }
+    // now consolidate -> add all nodes with same name together, add up counts but distinguish with different types of reached (or delete the ones without reached, if you have on with has reached?)
+        var pathListMap = mutableMapOf<NodeRed,Long>()
+        for (j in 0..pathList.size-1) {
+            var id = pathList[j].id
+            var count = pathList[j].count
+            var reached = pathList[j].reached
+
+            if (pathListMap.containsKey(NodeRed(id,reached))) {
+                pathListMap.put(NodeRed(id,reached), pathListMap.getValue(NodeRed(id,reached)) + count)
+            } else {
+                pathListMap.put(NodeRed(id,reached), count)
+            } 
+            pathList[j] = Node("",0,0)
+        } 
+        for ((key,count) in pathListMap) {
+            pathList.add(Node(key.id, count, key.reached))
+        }
+        while (pathList.contains(Node("",0,0))) {
+            pathList.removeAt(pathList.indexOf(Node("",0,0)))
+        } 
+        pathList.forEach {
+            if (it.id != "out") pathSearch = true
+        }
+    	
+	    //println(pathList)
+    }
+
+    
+    var result = -1L
+
+    println("xx $n")
+    pathList.forEach {
+        if (it.reached == 11) result = it.count
+    }
+    println(pathList)
+    return result
+}
+
 fun main() {
     
     var t1 = System.currentTimeMillis()
@@ -63,12 +157,12 @@ fun main() {
     println("   $solution1")
     println() 
 
-/*/  solution for part 2
-	val solution2 = factory(2)
+//  solution for part 2
+	val solution2 = reactor2(2)
     println("*******************************")
     println("Solution for part2")        
     println("   $solution2")
-    println() */
+    println()
 
     t1 = System.currentTimeMillis() - t1
     println("puzzle solved in ${t1} ms")
